eff494ccb8e397625f117bbce7cf5cfcc884447a
Maxim Kochnev <105023238+Maxim-Kochnev@users.noreply.github.com>
2025-12-08 23:13:45 +0500

    W1L6. Creating Player and Enemies

--- Assets/Scripts/Multiplayer/MultiplayerManager.cs
+++ Assets/Scripts/Multiplayer/MultiplayerManager.cs
@@ -1,7 +1,12 @@
 using Colyseus;
+using System;
+using UnityEngine;
 
 public class MultiplayerManager : ColyseusManager<MultiplayerManager>
 {
+    [SerializeField] private GameObject _player; 
+    [SerializeField] private GameObject _enemy; 
+
     private ColyseusRoom<State> _room;
 
     protected override void Awake()
@@ -15,6 +20,33 @@ public class MultiplayerManager : ColyseusManager<MultiplayerManager>
     public async void Connect()
     {
         _room = await Instance.client.JoinOrCreate<State>("state_handler");
+
+        _room.OnStateChange += OnChange;
+    }
+
+    private void OnChange(State state, bool isFirstState)
+    {
+        if (!isFirstState) return;
+
+        // создание на сцене игрока
+        var player = state.players[_room.SessionId];
+        InstantiatePlayer(_player, player);
+
+        // создание на сцене противников
+        state.players.ForEach(ForEachEnemy);
+    }
+
+    private void ForEachEnemy(string key, Player player)
+    {
+        if (key == _room.SessionId) return;
+
+        InstantiatePlayer(_enemy, player);
+    }
+
+    private void InstantiatePlayer(GameObject prefab, Player player)
+    {
+        Vector3 position = new Vector3(player.x - 200, 0, player.y - 200) / 8;
+        Instantiate(prefab, position, Quaternion.identity);
     }
 
     protected override void OnDestroy()
