78c0d57c73b4fd0b7927403214775391b6f1286f
Maxim Kochnev <105023238+Maxim-Kochnev@users.noreply.github.com>
2025-12-11 00:08:20 +0500

    W2 L2. Сглаживание перемещения

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,26 +4,68 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] private EnemyCharacter _character;
+    private List<float> _receiveTimeInterval = new List<float> { 0, 0, 0, 0, 0};
+    private float AverageInterval
+    {
+        get
+        {
+            int receiveTimeIntervalCount = _receiveTimeInterval.Count;
+            float sum = 0;
+            for (int i = 0; i < receiveTimeIntervalCount; i++)
+            {
+                sum += _receiveTimeInterval[i];
+            }
+
+            return sum / receiveTimeIntervalCount;
+        }
+    }
+
+    private float _lastReceiveTime = 0;
+
+    private void SaveReceiveTime()
+    {
+        float interval = Time.time - _lastReceiveTime;
+        _lastReceiveTime = Time.time;
+
+        _receiveTimeInterval.Add(interval);
+        _receiveTimeInterval.RemoveAt(0);
+    }
     internal void OnChange(List<DataChange> changes)
     {
-        Vector3 position = transform.position;
+        SaveReceiveTime();
+
+        Vector3 position = _character.TargetPosition;
+        Vector3 velocity = Vector3.zero;
 
         foreach (var dataChange in changes)
         {
             switch (dataChange.Field)
             {
-                case "x":
+                case "pX":
                     position.x = (float)dataChange.Value;
                     break;
-                case "y":
+                case "pY":
+                    position.y = (float)dataChange.Value;
+                    break;
+                case "pZ":
                     position.z = (float)dataChange.Value;
                     break;
+                case "vX":
+                    velocity.x = (float)dataChange.Value;
+                    break;
+                case "vY":
+                    velocity.y = (float)dataChange.Value;
+                    break;
+                case "vZ":
+                    velocity.z = (float)dataChange.Value;
+                    break;
                 default:
-                    Debug.LogWarning("Не обрабатывается изменение поля " + dataChange.Field);
+                    Debug.LogWarning("РќРµ РѕР±СЂР°Р±Р°С‚С‹РІР°РµС‚СЃСЏ РёР·РјРµРЅРµРЅРёРµ РїРѕР»СЏ " + dataChange.Field);
                     break;
             }
         }
 
-        transform.position = position;
+        _character.SetMovement(position, velocity, AverageInterval);
     }
 }
