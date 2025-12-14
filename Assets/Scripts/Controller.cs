45cecb899adf1f7f928e8aac6f1ae63805ab95a7
Maxim Kochnev <105023238+Maxim-Kochnev@users.noreply.github.com>
2025-12-14 20:50:02 +0500

    W2 L6. Выстрел плеера

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     [SerializeField] private PlayerCharacter _player;
+    [SerializeField] private PlayerGun _gun;
     [SerializeField] private float _mouseSensetivity = 2f;
+    private MultiplayerManager _multiplayerManager;
+
+    private void Start()
+    {
+        _multiplayerManager = MultiplayerManager.Instance;
+    }
 
     private void Update()
     {
@@ -14,14 +21,27 @@ public class Controller : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
 
+        bool isShoot = Input.GetMouseButton(0);
+
         bool space = Input.GetKeyDown(KeyCode.Space);
 
         _player.SetInput(h, v, mouseX);
         _player.RotateX(-mouseY * _mouseSensetivity);
         if (space) _player.Jump();
+        
+        if (isShoot && _gun.TryShoot(out ShootInfo shootInfo)) SendShoot(ref shootInfo);
 
         SendMove();
     }
+
+    private void SendShoot(ref  ShootInfo shootInfo)
+    {
+        shootInfo.key = _multiplayerManager.GetSessionID();
+        string json = JsonUtility.ToJson(shootInfo);
+
+        _multiplayerManager.SendMessage("shoot", json);
+    }
+
     private void SendMove()
     {
         _player.GetMoveInfo(out Vector3 position, out Vector3 velocity, out float rotateX, out float rotateY);
@@ -36,6 +56,18 @@ public class Controller : MonoBehaviour
             { "rX", rotateX },
             { "rY", rotateY }
         };
-        MultiplayerManager.Instance.SendMessage("move", data);
+        _multiplayerManager.SendMessage("move", data);
     }
+}
+
+[System.Serializable]
+public struct ShootInfo
+{
+    public string key;
+    public float pX;
+    public float pY;
+    public float pZ;
+    public float dX;
+    public float dY;
+    public float dZ;
 }
